52651960907b7e36719f26ac5ebc59cb175b942d
Tammy Gagliano <tracsd@gmail.com>
2021-11-13 17:25:01 -0600

    update to service worker files to add events

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,7 @@
 var CACHE_NAME = 'my-site-cache-v1';
+// Try to get response from a cache
+const DATA_CACHE_NAME = "data-cache-v1";
+
 var urlsToCache = [
   '/',
   '/public/db.js',
@@ -21,21 +24,43 @@ self.addEventListener('install', function(event) {
   );
 });
 
+
 // Return a cached response with fetch
-self.addEventListener('fetch', function(event) {
-    // cache all get requests 
-    if (){
-        
+self.addEventListener("fetch", function(event) {
+    // cache all get requests to /api routes
+    if (event.request.url.includes("/api/")) {
+      event.respondWith(
+        caches.open(DATA_CACHE_NAME).then(cache => {
+          return fetch(event.request)
+            .then(response => {
+              // If the response was good, clone it and store it in the cache.
+              if (response.status === 200) {
+                cache.put(event.request.url, response.clone());
+              }
+  
+              return response;
+            })
+            .catch(err => {
+              // Network request failed, try to get it from the cache.
+              return cache.match(event.request);
+            });
+        }).catch(err => console.log(err))
+      );
+  
+      return;
     }
+
     event.respondWith(
-      caches.match(event.request)
-        .then(function(response) {
-          // Cache hit - return response
-          if (response) {
-            return response;
-          }
-          return fetch(event.request);
-        }
-      )
-    );
-  });
+        fetch(event.request).catch(function() {
+          return caches.match(event.request).then(function(response) {
+            if (response) {
+              return response;
+            } else if (event.request.headers.get("accept").includes("text/html")) {
+              // return the cached home page for all requests for html pages
+              return caches.match("/");
+            }
+          });
+        })
+      );
+    });
+    
